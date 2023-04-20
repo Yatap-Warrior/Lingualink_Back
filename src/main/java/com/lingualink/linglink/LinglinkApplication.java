497e331f6cb4e4d429e33652faab2049a8d33149
gardenee <snnjjang@naver.com>
2023-04-20 22:30:28 +0900

    first_commit

---
+++ src/main/java/com/lingualink/linglink/LinglinkApplication.java
@@ -0,0 +1,13 @@
+package com.lingualink.linglink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinglinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinglinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lingualink/linglink/LinglinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingualink.linglink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinglinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
