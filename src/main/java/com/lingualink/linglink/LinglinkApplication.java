cce8856a5ee955a30f442737818e316cc56f93b1
gardenee <snnjjang@naver.com>
2023-04-30 00:20:40 +0900

    spring security 임시 배제

--- src/main/java/com/lingualink/linglink/LinglinkApplication.java
+++ src/main/java/com/lingualink/linglink/LinglinkApplication.java
@@ -2,11 +2,12 @@ package com.lingualink.linglink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @EnableEncryptableProperties
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class LinglinkApplication {
 
 	public static void main(String[] args) {
